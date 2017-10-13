82acd0a1e78a3eed2a84ac9442245fb83f0202b9
Phoenix Zerin <phx@phx.ph>
2017-10-14 12:20:33 +1300

    Bumping version number.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
   name        = 'PyOTA-CCurl',
   description = 'C Curl extension for PyOTA',
   url         = 'https://github.com/todofixthis/pyota-ccurl',
-  version     = '1.0.5',
+  version     = '1.0.6',
 
   long_description = long_description,
 
